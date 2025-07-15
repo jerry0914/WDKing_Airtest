33b4e5b4ff2dcf5587cedfa03a2a144eb64e24ac
jerryhuang <jerryhuang@wdkingtech.com>
2025-07-15 20:28:35 +0800

    update

--- MainLobby.air/MainLobby.py
+++ MainLobby.air/MainLobby.py
@@ -9,6 +9,8 @@ import urllib.request
 
 auto_setup(__file__)
 
+total_retry:int=0
+
 def launch_app():
     package_name = "com.wudiking.wd_dev"
     activity_name = "com.unity3d.player.UnityPlayerActivity"
@@ -52,24 +54,45 @@ def _trial_player_login()->bool:
               touch(result)
     return result is not False
 
-def _close_ad(lastResult:bool = False)->bool:
-    target = _waitForExists(Template(r"tpl1751954040752.png", record_pos=(0.002, 1.013), resolution=(1080, 2400)),retry=2)
-    result = lastResult | (target is not False)
-    if target:
-        touch(target)
-        sleep(1)
-        _close_ad(True)
-    return result
+# def _close_ad(lastResult:bool = False)->bool:
+#     target = _waitForExists(Template(r"tpl1751954040752.png", record_pos=(0.002, 1.013), resolution=(1080, 2400)),retry=2)
+#     result = lastResult | (target is not False)
+#     if target:
+#         touch(target)
+#         sleep(1)
+#         _close_ad(True)
+# #     return result
        
-def _close_navi(lastResult:bool = False)->bool:
-    target = _waitForExists(Template(r"tpl1751954806599.png", record_pos=(0.005, 1.049), resolution=(1080, 2400)),retry=2)
-    result = lastResult | (target is not False)
-    if target:
-        touch(target)
-        sleep(1)
-        _close_navi(True)
-    return result
+# def _close_navi(lastResult:bool = False)->bool:
+#     target = _waitForExists(Template(r"tpl1751954806599.png", record_pos=(0.005, 1.049), resolution=(1080, 2400)),retry=2)
+#     result = lastResult | (target is not False)
+#     if target:
+#         touch(target)
+#         sleep(1)
+#         _close_navi(True)
+#     return result
 
+def waitForFileDownload(cnt:int=0):
+    imgDownload=Template(r"tpl1752496216507.png", record_pos=(0.129, 0.949), resolution=(1080, 2400))
+    imgUnzip=Template(r"tpl1752547747474.png", record_pos=(0.145, 0.947), resolution=(1080, 2400))
+    imgNetworkFail=Template(r"tpl1752579921135.png", record_pos=(-0.195, 0.163), resolution=(1080, 2400))
+    isNetworkFail=exists(imgNetworkFail)
+    if isNetworkFail:
+        touch(isNetworkFail)
+        sleep(10)
+        Ready_To_Play(total_retry+1)
+        return False
+#     assert_equal(isNetworkFail,False,"網路異常")
+    assert_equal(cnt<300,True,"下載逾時")
+    isDownloading=exists(imgDownload)
+    if isDownloading:
+        waitForFileDownload(cnt+1)
+        return True
+    isUnzip=exists(imgUnzip)
+    if isUnzip:
+        waitForFileDownload(cnt+1)
+        return True
+    
 def _waitForExists(v,retry:int=1):
     result=False
     for i in range(0, retry):
@@ -78,29 +101,54 @@ def _waitForExists(v,retry:int=1):
             break
     return result
 
+def _waitForNotExists(v,retry=100):
+    result=False
+    for i in range(0, retry):
+        result = exists(v)
+        if result is False:
+            break
+    return (result==False)
+
 def _waitAndTouchTarget(v,retry:int=1):
     target = _waitForExists(v,retry)
     if target:
         touch(target)
     return target
-
-def close_ad_and_nave():
-    result  = _close_ad()
-    result |= _close_navi()
-    if result:
+    result=False
+    
+def close_ad_and_nave(retry:int = 0):
+    sleep(1)
+    img_x=Template(r"tpl1751954040752.png", record_pos=(0.002, 1.013), resolution=(1080, 2400))
+    target_x=exists(img_x)
+    if target_x:
+        touch(target_x)
         close_ad_and_nave()
+        return
+    img_arrow=Template(r"tpl1751954806599.png", record_pos=(0.005, 1.049), resolution=(1080, 2400))
+    target_arrow=exists(img_arrow)
+    if target_arrow:
+        touch(target_arrow)
+        close_ad_and_nave()
+        return
+    if retry < 2:
+        close_ad_and_nave(retry+1)
               
-def Ready_To_Play():
+def Ready_To_Play(retry:int=0):
+    total_retry=retry
+    assert_not_equal(total_retry>=3,True,"Total retry >3,測試失敗")
     launch_app()
     sleep(50)
     is_app_start = _check_app_start()
     if is_app_start is "download": 
         _click_data_download()
-        sleep(100)
+        sleep(2)
+        waitForFileDownload()
         _enter_password()
+        sleep(30)
         _trial_player_login()
         close_ad_and_nave()
     elif is_app_start == "ad":
+        sleep(20)
         close_ad_and_nave()
     check=_waitAndTouchTarget(Template(r"tpl1751973367903.png", record_pos=(-0.275, 0.007), resolution=(1080, 2400)),retry=10)
     assert_not_equal(check,False)
@@ -110,6 +158,7 @@ def Ready_To_Play():
     
 def check_lobby_ready():
     return _waitForExists(Template(r"tpl1752222802882.png", record_pos=(-0.354, 0.869), resolution=(1080, 2400)),retry=3)
+
 
 def Click_SG_Tab():
     return _waitAndTouchTarget(Template(r"tpl1752226110055.png", record_pos=(-0.002, 0.87), resolution=(1080, 2400)))
--- sp1006.air/sp1006.py
+++ sp1006.air/sp1006.py
@@ -7,39 +7,60 @@ import MainLobby
 
 auto_setup(__file__)
 
-def _check_large_icon():   
-    return MainLobby._waitForExists(Template(r"tpl1752226971986.png", record_pos=(0.195, 0.264), resolution=(1080, 2400)),retry=3)
+large_icon_base=Template(r"tpl1752228461658.png")
+large_icon_ready=Template(r"tpl1752228461658.png")
+small_icon_base=Template(r"tpl1752228461658.png")
+small_icon_ready=Template(r"tpl1752231412310.png")
+
+
+def _check_large_icon():
+    return MainLobby._waitForExists(large_icon_base,retry=1)
 
 def _check_small_icon():
-    return MainLobby._waitForExists(Template(r"tpl1752226971986.png", record_pos=(0.195, 0.264), resolution=(1080, 2400)),retry=3)
+    return MainLobby._waitForExists(small_icon_base,retry=1)
+
+def _swipe_to_top():
+    for i in range(0,10):
+        swipe((500,1500),vector=[0.01, 0.3])
+        sleep(1)
 
 def search_game_icon():
+    _swipe_to_top()
     target = _check_large_icon()
     if target is False:
         for i in range(0,10):
             target = _check_small_icon()
             if target:
-                
+                break
             else:
-                
-
-
+                swipe((500,1500),vector=[0.01, -0.3])
     return target
 
-    
-def spin_test():
-    if exists(Template(r"tpl1727860219029.png", threshold=0.95, rgb=True, record_pos=(0.311, -0.189), resolution=(1080, 1920))):
-        touch(Template(r"tpl1727860219029.png", record_pos=(0.305, -0.19), resolution=(1080, 1920)))
-    wait (Template(r"tpl1727860295719.png", threshold=0.9, rgb=True, record_pos=(0.313, -0.19), resolution=(1080, 1920)),timeout=120, interval=3)
-    touch(Template(r"tpl1727860295719.png", record_pos=(0.305, -0.19), resolution=(1080, 1920)))    
-    common.wait_game_info_button()
-    if exists(Template(r"tpl1727860432224.png", record_pos=(-0.001, 0.047), resolution=(1080, 1920))):
-        touch(Template(r"tpl1727860432224.png", record_pos=(-0.001, 0.057), resolution=(1080, 1920)))
-    common.wait_enter_into_game()
-    common.check_close_dialog()
-    for i in range(0,10):
-        wait(Template(r"tpl1727860576436.png", threshold=0.95, rgb=True, record_pos=(-0.001, 0.715), resolution=(1080, 1920)),timeout=120,interval=3)
-        print("第{}次Spin".format(i+1))
-        touch(Template(r"tpl1727860576436.png", record_pos=(-0.003, 0.705), resolution=(1080, 1920)))
-        common.in_game_wait_go_back(timeout=300)
+def wait_download_and_click_small():
+    target = MainLobby._waitForExists(small_icon_ready,retry=1)
+    touch(small_icon_base)
+    if target is False:
+        # waiting for download...
+        target = MainLobby._waitForExists(small_icon_base,retry=180)
+    if target:
+        touch(small_icon_base)
+    return target
 
+def Spin_Test():
+    assert search_game_icon()
+    assert wait_download_and_click_small()
+    _run_spin_test_base()
+    assert MainLobby.GoBackHallInGame();
+
+    
+
+    
+def _run_spin_test_base():
+    MainLobby.Close_FirstSpinMsgBox();
+    assert MainLobby._waitAndTouchTarget(Template(r"tpl1752232270905.png", record_pos=(0.003, 0.933), resolution=(1080, 2400)),retry=2)
+    sleep(3)
+    assert MainLobby._waitAndTouchTarget(Template(r"tpl1752232270905.png", record_pos=(0.003, 0.933), resolution=(1080, 2400)),retry=15)
+    
+    
+Spin_Test()
+    
